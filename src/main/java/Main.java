e426713e121c5f59ef824ab7504c7068e968a9ad
dudezi <a01036952858@gmail.com>
2024-08-20 09:46:29 +0900

    bbb

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -13,7 +14,10 @@ public class Main {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
-                
+                System.out.print("제목 : ");
+                String subject = sc.nextLine();
+                System.out.print("내용 : ");
+                String content = sc.nextLine();
             }
         }
     }
