b737072e78aa9943e39c24bfb23497f6aa323631
dudezi <a01036952858@gmail.com>
2024-08-20 09:42:01 +0900

    third

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("== 게시판 앱 ==");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.printf("명령) ");
+            String command = sc.nextLine();
+            if (command.equals("종료")) {
+                break;
+            } else if (command.equals("등록")) {
+                
+            }
+        }
     }
 }
