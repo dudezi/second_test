d1f8dc98090da4f64b39a57164abc3dcab344f42
dudezi <a01036952858@gmail.com>
2024-08-20 10:21:35 +0900

    5단계_내림차순ê¹

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +14,8 @@ public class App {
 
         int lastId = 1;
 
+        List<Article> articleList = new ArrayList<>();
+
         while (true) {
             System.out.printf("명령) ");
             String command = sc.nextLine().trim();
@@ -24,7 +28,19 @@ public class App {
                 System.out.print("내용 : ");
                 String content = sc.nextLine().trim();
                 System.out.printf("%d번게시물이 등록되었습니다.\n", lastId);
+
+                Article article = new Article(lastId, subject, content);
+                articleList.add(article);
+
                 lastId++;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 제목 / 내용");
+                System.out.println("----------------------");
+
+                for(int i = articleList.size()-1; i >= 0 ; i--){
+                    Article article = articleList.get(i);
+                    System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
+                }
             }
         }
     }
