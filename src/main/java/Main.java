4a5c09e26526a4113d9c23a0e16c3de709b0d6b6
dudezi <a01036952858@gmail.com>
2024-08-20 09:40:15 +0900

    secend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,5 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("== 게시판 앱 ==");
-
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.printf("명령) ");
-            String command = sc.nextLine();
-            if (command.equals("종료")) {
-                break;
-            }
-        }
     }
 }
