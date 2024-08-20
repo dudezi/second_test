391318e94978fe347f504086579b84aa4cae2f2e
dudezi <a01036952858@gmail.com>
2024-08-20 09:53:06 +0900

    4단ã계

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,21 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        int lastId = 1;
+
         while (true) {
             System.out.printf("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
+
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
                 System.out.print("제목 : ");
-                String subject = sc.nextLine();
+                String subject = sc.nextLine().trim();
                 System.out.print("내용 : ");
-                String content = sc.nextLine();
+                String content = sc.nextLine().trim();
+                System.out.printf("%d번게시물이 등록되었습니다.\n", lastId);
+                lastId++;
             }
         }
     }
